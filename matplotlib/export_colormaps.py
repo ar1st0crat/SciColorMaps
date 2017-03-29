d553498a4c92e8b21794f24fd4ab769abd74e1c8
ar1st0crat <art1st.prog@gmail.com>
2017-03-29 22:49:08 +0300

    add python script for exporting matplotlib colormaps to text files

---
+++ matplotlib/export_colormaps.py
@@ -0,0 +1,36 @@
+import os
+import matplotlib.pyplot as plt
+
+
+DATADIR = 'colormaps'
+
+os.mkdir(DATADIR)
+
+# get all colormaps available in matplotlib
+# (except those that end with '_r')
+maps = [m for m in plt.colormaps() if not m.endswith('_r')]
+
+# save colormaps (each to separate .txt file)
+for name in maps:
+    # e.g. "colormaps/terrain.txt"
+    filename = os.path.join(DATADIR, name + '.txt')
+
+    with open(filename, 'wt') as f:
+        # get all colors in colormap
+        cmap = plt.get_cmap(name)
+
+        for i in range(256):
+            # format RGB as {r, g, b}, {r, g, b}, etc.
+            # so it could easily be copied to C# code
+            r = int(cmap(i)[0] * 255)
+            g = int(cmap(i)[1] * 255)
+            b = int(cmap(i)[2] * 255)
+            rgb = '{%3d, %3d, %3d}, ' % (r, g, b)
+            # four RGB tuples per line
+            if (i + 1) % 4 == 0:
+                rgb += '\n'
+            # append to file
+            f.write(rgb)
+
+        # log
+        print('Exported to file: ' + name)
